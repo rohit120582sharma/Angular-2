24c52b46b9511c2ba1885a0a483bffc1f7b1f5ea
Rohit Sharma <rohitsharma120582@gmail.com>
2016-12-07 01:08:09 +0530

    fototags: karma initialization

---
+++ fototags/unit/first.spec.js
@@ -0,0 +1,8 @@
+describe("when directive is complied, it ", function(){
+
+	var  config = true;
+	it("should should exists", function(){
+		var jackie = true;
+		expect(jackie).toBe(false);
+	})
+})
